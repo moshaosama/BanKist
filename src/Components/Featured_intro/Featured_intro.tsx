bffb2cda69302718e36c4e8ea47360eca7c902d3
moshaosama <mohamedOSFekry@gmail.com>
2025-06-14 19:16:48 +0300

    modified: Featured_intro.tsx

--- src/Components/Featured_intro/Featured_intro.tsx
+++ src/Components/Featured_intro/Featured_intro.tsx
@@ -1,12 +1,12 @@
 import { globalStyle } from "../../Styles";
 import featureStyle from "./Featured_intro.module.css";
 const Featured_intro = () => {
-  const { container } = globalStyle();
+  const { container, intro_content } = globalStyle();
   const { header_section } = featureStyle;
   return (
     <>
       <div className={container}>
-        <div className="flex flex-col justify-start items-start w-[35pc] mx-auto">
+        <div className={intro_content}>
           <h1 className={header_section}>Features</h1>
           <p className="text-3xl font-bold">
             Everything you need in a modern bank and more
