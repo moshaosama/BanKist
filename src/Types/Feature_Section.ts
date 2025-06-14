cd2cdc2e308e9443e45e11a6e1e6016f6e846480
moshaosama <mohamedOSFekry@gmail.com>
2025-06-14 18:57:28 +0300

    added: Types folder

---
+++ src/Types/Feature_Section.ts
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export interface Feature_Section_Props {
+  Image: string;
+  Icon: ReactElement;
+  Header: string;
+  description: string;
+  flexDirection: string;
+}
