df2d8f198567828b3bc77ecf97d6ac864bf5c895
moshaosama <mohamedOSFekry@gmail.com>
2025-06-14 19:22:11 +0300

    modified: Home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,6 +3,7 @@ import HeroSection from "../../Components/HeroSection/HeroSection";
 import { Header } from "../../Layouts/Header/Header";
 import Features_Section_Render from "../../Components/Featured_Section/Components/Features_Section_Render";
 import Operation_Intro from "../../Components/Operations/Components/Operation_Intro";
+import Operation_Section from "../../Components/Operations/Components/Operation_Section";
 
 export const Home = () => {
   return (
@@ -12,6 +13,7 @@ export const Home = () => {
       <Featured_intro />
       <Features_Section_Render />
       <Operation_Intro />
+      <Operation_Section />
     </>
   );
 };
