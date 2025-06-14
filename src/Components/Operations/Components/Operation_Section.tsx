8488a2961ba70ea896a53291f3fa75d854203259
moshaosama <mohamedOSFekry@gmail.com>
2025-06-14 19:22:11 +0300

    added: Operation_Section.tsx

---
+++ src/Components/Operations/Components/Operation_Section.tsx
@@ -0,0 +1,11 @@
+const Operation_Section = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <h1>sjadlas</h1>
+      </div>
+    </>
+  );
+};
+
+export default Operation_Section;
