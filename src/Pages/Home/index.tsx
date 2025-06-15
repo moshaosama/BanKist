fd3232483dd6ad6fa7fe3d9a98ea95c9d979b31a
moshaosama <mohamedOSFekry@gmail.com>
2025-06-15 18:00:31 +0300

    modified: Home page layout

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,19 +1,19 @@
-import Featured_intro from "../../Components/Featured_intro/Featured_intro";
 import HeroSection from "../../Components/HeroSection/HeroSection";
 import { Header } from "../../Layouts/Header/Header";
 import Features_Section_Render from "../../Components/Featured_Section/Components/Features_Section_Render";
-import Operation_Intro from "../../Components/Operations/Components/Operation_Intro";
 import Operation_Section from "../../Components/Operations/Components/Operation_Section";
+import SureSection from "../../Components/Sure/SureSection";
+import Footer from "../../Layouts/Footer/Footer";
 
 export const Home = () => {
   return (
     <>
       <Header />
       <HeroSection />
-      <Featured_intro />
       <Features_Section_Render />
-      <Operation_Intro />
       <Operation_Section />
+      <SureSection />
+      <Footer />
     </>
   );
 };
