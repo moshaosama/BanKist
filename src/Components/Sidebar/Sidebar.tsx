cafb912b89dc641a2e319449fdfe16be209728f7
moshaosama <mohamedOSFekry@gmail.com>
2025-06-15 19:49:30 +0300

    added: Sidebar components folder

---
+++ src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+import { useSidebarContext } from "../../Context/SidebarContext";
+import { HiMiniXMark } from "react-icons/hi2";
+import styleHeader from "../../Layouts/Header/Header.module.css";
+
+const Sidebar = () => {
+  const { isOpen, handleTriggerSidebar } = useSidebarContext();
+  const { button, link_text } = styleHeader;
+  return (
+    <>
+      <div
+        className={clsx(
+          "bg-white fixed h-full z-50 top-0 overflow-hidden transition-all duration-500",
+          isOpen ? "w-full" : "w-0"
+        )}
+      >
+        <div className="flex justify-end" onClick={handleTriggerSidebar}>
+          <HiMiniXMark size={80} />
+        </div>
+        <div className="flex flex-col justify-center items-center gap-10 mt-36">
+          <h1 className={link_text}>Features</h1>
+          <h1 className={link_text}>Operations</h1>
+          <h1 className={link_text}>Testimonials</h1>
+          <button className={button}>Open Account</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
